3a3cefd5525f2fbc347a1ea7ddf288e28469853a
Serban Ghita <sghita@adobe.com>
2017-03-27 13:08:55 +0300

    Fixed Maven build.

---
+++ build/AMD/dist/js/bundle.amd.js
@@ -0,0 +1,64 @@
+// interface Window {
+//     attachEvent(event: string, listener: EventListener): boolean;
+//     detachEvent(event: string, listener: EventListener): void;
+// }
+define("common/Utils", ["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var Utils = (function () {
+        function Utils() {
+        }
+        Utils.prototype.randomInt = function (min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        };
+        Utils.prototype.ready = function (fn) {
+            if (document.addEventListener) {
+                // Use the handy event callback
+                // document.addEventListener( "DOMContentLoaded", fn, false );
+                // A fallback to window.onload, that will always work
+                window.addEventListener("load", fn, false);
+                // If IE event model is used
+            }
+            else {
+                // Ensure firing before onload, maybe late but safe also for iframes
+                // document.attachEvent( "onreadystatechange", fn );
+                // A fallback to window.onload, that will always work
+                window.attachEvent("onload", fn);
+            }
+        };
+        return Utils;
+    }());
+    exports.Utils = Utils;
+});
+define("client/Client", ["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var Client = (function () {
+        function Client(utils) {
+            this.utils = utils;
+        }
+        Client.prototype.isNode = function () {
+            return (typeof document === "undefined");
+        };
+        Client.prototype.hello = function () {
+            var msg = "Hello there! Random: " + this.utils.randomInt(0, 100);
+            if (this.isNode()) {
+                console.log(msg);
+            }
+            else {
+                this.utils.ready(function () {
+                    document.body.innerHTML = msg;
+                });
+            }
+        };
+        return Client;
+    }());
+    exports.Client = Client;
+});
+define("index", ["require", "exports", "client/Client", "common/Utils"], function (require, exports, Client_1, Utils_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var u = new Utils_1.Utils();
+    var c = new Client_1.Client(u);
+    c.hello();
+});
