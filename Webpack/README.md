eea0b36e41b2c34617d068aad97bf1408a625427
Serban Ghita <sghita@adobe.com>
2018-03-13 16:02:11 +0200

    TypeScript codebased transpiled and bundled with Webpack.

---
+++ Webpack/README.md
@@ -0,0 +1,12 @@
+*Description*
+
+* TypeScript project.
+* SystemJS module style.
+* ES5 code output.
+* JS output bundled by Webpack.
+
+*Install*
+
+* `npm install`
+* `npm run build`
+* `npm run dev` to start the server at `http://localhost:3000`
---
+++ Webpack/webpack.config.js
@@ -0,0 +1,40 @@
+var path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist/js"),
+    publicPath: "/js/"
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "eval-source-map",
+
+  devServer: {
+    contentBase: "dist",
+    watchContentBase: true
+  },
+
+  watch: false,
+  node: {
+    __dirname: true
+  },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      {
+        test: /\.ts$/,
+        loader: "awesome-typescript-loader"
+      },
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  }
+};
