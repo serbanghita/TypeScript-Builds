f0fe0430286e6c229a4bf15b2c3efbd45002517c
Victor Glindås <glinkis@gmail.com>
2018-03-14 16:34:54 +0100

    Update webpack.config.js
    Adds `*.tsx`-files to be processed by a the loader.
    Expands other loader for consistency.

--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -29,12 +29,16 @@ module.exports = {
     rules: [
       // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
       {
-        test: /\.ts$/,
+        test: /\.(ts|tsx)$/,
         loader: "awesome-typescript-loader"
       },
 
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
     ]
   }
 };
